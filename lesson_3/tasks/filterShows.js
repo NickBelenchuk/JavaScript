375f062db5ee2d7bddf5b9a02bec086acafcd035
Nick <nick.belenchuk@gmail.com>
2023-04-21 21:46:17 +0300

    updated filter

--- lesson_3/tasks/filterShows.js
+++ lesson_3/tasks/filterShows.js
@@ -8,8 +8,6 @@ filterByYear + filterByTitle = one function
 For example, filterShows(shows, filterCriteria);
 */
 
-// це шок, те що тут написано((
-
 export const filterShows = (shows, filterCriteria) => {
   const { year, title, rating } = filterCriteria;
   const filteredShows = shows.filter((show) => {
@@ -25,11 +23,14 @@ export const filterShows = (shows, filterCriteria) => {
 };
 
 export const shows = [
-  { title: "Bad boys", year: 2222, rating: 5 },
-  { title: "Good girls", year: 3333, rating: 8 },
-  { title: "The cringe", year: 4141, rating: 7 },
-  { title: "Real cringe", year: 1111, rating: 6 },
-  { title: "Lord of the rings", year: 2121, rating: 9 },
+  { title: "Bad boys", year: 1881, rating: 5 },
+  { title: "Good girls", year: 2019, rating: 8 },
+  { title: "The cringe", year: 2001, rating: 7 },
+  { title: "Real cringe", year: 2008, rating: 6 },
+  { title: "Lord of the rings", year: 2021, rating: 9 },
+  { title: "The word", year: 2019, rating: 3 },
+  { title: "Any idea", year: 2006, rating: 5 },
+  { title: "True Detective", year: 2023, rating: 10 },
 ];
 
-export const filterCriteria = { year: 2016, title: "the", rating: 7 };
+export const filterCriteria = { year: 2016, title: "the", rating: 6 };
